87266647db9e1d23f3637cc06898188d2766f637
Krishna12317 <krishna12317@hotmail.com>
2024-06-14 22:00:19 +0530

    fix: added a separate component to handle note list functionalities

---
+++ src/components/notelist/NoteListContainer.tsx
@@ -0,0 +1,70 @@
+// StyledNoteListContainer.tsx
+import React, { useCallback, useState } from "react";
+import { RiFileList3Line } from "react-icons/ri";
+import { useTranslation } from "react-i18next";
+import { Note } from "../../types";
+import NoteList from "./NoteList";
+import NoteInput from "./NoteInput";
+import {
+  StyledEmptyNoteContainer,
+  StyledNoteListContainer,
+} from "../../styles";
+
+const NoteListContainer: React.FC = () => {
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [pinnedNotes, setPinnedNotes] = useState<Note[]>([]);
+  const { t: translate } = useTranslation();
+
+  const handleAddingNote = useCallback(
+    (note: Note) => {
+      setNotes([...notes, note]);
+    },
+    [notes]
+  );
+
+  const togglePin = useCallback(
+    (note: Note) => {
+      if (pinnedNotes.includes(note)) {
+        setPinnedNotes(pinnedNotes.filter((n) => n !== note));
+        setNotes([...notes, note]);
+      } else {
+        setPinnedNotes([...pinnedNotes, note]);
+        setNotes(notes.filter((n) => n !== note));
+      }
+    },
+    [notes, pinnedNotes]
+  );
+
+  return (
+    <>
+      <NoteInput onAddNote={handleAddingNote} />
+      <StyledNoteListContainer>
+        {pinnedNotes.length > 0 ? (
+          <NoteList
+            title={translate("pinned")}
+            notes={pinnedNotes}
+            setNotes={setPinnedNotes}
+            togglePin={togglePin}
+            isPinned
+          />
+        ) : null}
+        {notes.length > 0 ? (
+          <NoteList
+            title={translate("notes")}
+            notes={notes}
+            setNotes={setNotes}
+            togglePin={togglePin}
+            isPinned={false}
+          />
+        ) : (
+          <StyledEmptyNoteContainer>
+            <RiFileList3Line size={48} />
+            <p>{translate("emptyNotes")}</p>
+          </StyledEmptyNoteContainer>
+        )}
+      </StyledNoteListContainer>
+    </>
+  );
+};
+
+export default React.memo(NoteListContainer);
